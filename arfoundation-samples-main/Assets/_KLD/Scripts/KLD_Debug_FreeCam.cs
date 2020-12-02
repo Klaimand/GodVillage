f4bb5fa31a47fe3583dff200fa038f6433cd7cd9
Clément Pagnier <clempagnier@yahoo.fr>
2020-12-02 16:38:23 +0100

    [MERGEABLE] KLD_Debug_Camera script a ajouter sur la cam

---
+++ arfoundation-samples-main/Assets/_KLD/Scripts/KLD_Debug_FreeCam.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KLD_Debug_FreeCam : MonoBehaviour
+{
+
+    [SerializeField]
+    float speed = 1f;
+
+    [SerializeField]
+    float sensitivity = 30f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        translateCam();
+
+        if (Input.GetButton("Fire1"))
+        {
+            rotateCam();
+        }
+    }
+
+    void translateCam()
+    {
+        transform.position += new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("ThirdAxis"), Input.GetAxisRaw("Vertical")) * speed * Time.deltaTime;
+    }
+
+    void rotateCam()
+    {
+        transform.Rotate(new Vector3(Input.GetAxisRaw("Mouse Y"), Input.GetAxisRaw("Mouse X") * -1f, 0f) * sensitivity * 10f * Time.deltaTime);
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, 0f);
+    }
+}
