97b85c7d5d7351db9a5272ab6fd1f9ca1c66cf2a
Clément Pagnier <clempagnier@yahoo.fr>
2020-12-02 10:00:30 +0100

    import du projet inital

---
+++ arfoundation-samples-main/Assets/_KLD/Scripts/KLD_VillagePlacer.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation.Samples;
+
+public class KLD_VillagePlacer : MonoBehaviour
+{
+
+    [SerializeField]
+    PlaceOnPlane placeOnPlane;
+    bool isPlacingVillage = false;
+
+
+    GameObject spawnedObjInst;
+    KLD_TouchScaler curTouchScaler;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isPlacingVillage = true;
+        placeOnPlane.enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        /*
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            validatePosition();
+        }*/
+    }
+
+    public void validatePosition()
+    {
+        isPlacingVillage = false;
+        placeOnPlane.enabled = false;
+        //getCurVillageInst();
+
+        spawnedObjInst = placeOnPlane.spawnedObject;
+
+        if (spawnedObjInst != null)
+        {
+            curTouchScaler = spawnedObjInst.GetComponent<KLD_TouchScaler>();
+            curTouchScaler.isScalable = true;
+
+        }
+        else
+        {
+            Debug.LogError("there is no spawnedobj");
+        }
+
+
+    }
+
+    void getCurVillageInst()
+    {
+        if (isPlacingVillage && placeOnPlane.spawnedObject != null)
+        {
+            spawnedObjInst = placeOnPlane.spawnedObject;
+        }
+    }
+
+
+}
