2152c56d572e424ec975a7aa03416ccc11d69f8c
Clément Pagnier <clempagnier@yahoo.fr>
2020-12-02 18:42:50 +0100

    mise en place des fonctions pour arreter le calcul et désactiver la simu

---
+++ arfoundation-samples-main/Assets/_KLD/Scripts/KLD_MenuFunctions.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+
+public class KLD_MenuFunctions : MonoBehaviour
+{
+
+    [SerializeField]
+    Transform arSessionOrigin;
+
+    ARPlaneManager planeManager;
+    ARPointCloudManager pointCloudManager;
+
+    bool isArDebugVisible = true;
+
+    private void Start()
+    {
+        planeManager = arSessionOrigin.GetComponent<ARPlaneManager>();
+        pointCloudManager = arSessionOrigin.GetComponent<ARPointCloudManager>();
+    }
+
+    private void Update()
+    {
+        if (!isArDebugVisible)
+        {
+            foreach (var plane in planeManager.trackables)
+            {
+                plane.gameObject.SetActive(false);
+            }
+            foreach (var point in pointCloudManager.trackables)
+            {
+                point.gameObject.SetActive(false);
+            }
+            //print("p");
+        }
+    }
+
+    public void disablePlaneVisibility()
+    {
+        isArDebugVisible = false;
+    }
+
+    public void stopArCalculations()
+    {
+        planeManager.enabled = false;
+        pointCloudManager.enabled = false;
+    }
+
+    public void disableARDebugVisibility()
+    {
+        foreach (var plane in planeManager.trackables)
+        {
+            plane.gameObject.SetActive(false);
+        }
+        planeManager.enabled = false;
+
+        foreach (var point in pointCloudManager.trackables)
+        {
+            point.gameObject.SetActive(false);
+        }
+        pointCloudManager.enabled = false;
+
+    }
+
+
+}
