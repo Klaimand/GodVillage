459787d5dbc49a3adad7bc215e1390ef6941458f
Ryo207 <b.decarvalho@rubika-edu.com>
2020-12-03 10:43:43 +0100

    Mining Minigame

---
+++ arfoundation-samples-main/Assets/_BDC/BDC_MiningMinigame.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class BDC_MiningMinigame : MonoBehaviour
+{
+
+    public UnityEvent onClickMining;
+    public GameObject canvasMining;
+    public bool miningPhase;
+    public int numberOfTouchRequiredMining = 10;
+    public int miningTouchCount;
+
+
+    void Update()
+    {
+        Touch touch = Input.GetTouch(0);
+
+        if (miningPhase == true)
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                miningTouchCount++;
+
+                if (miningTouchCount > numberOfTouchRequiredMining)
+                {
+                    canvasMining.GetComponent<SpriteRenderer>().enabled = false;
+                    miningPhase = false;
+                    miningTouchCount = 0;
+                }   
+            }
+
+        }
+    }
+
+    public void MiningMinigame()
+    {
+        canvasMining.GetComponent<SpriteRenderer>() .enabled = true;
+        miningPhase = true;
+
+    }
+
+}
